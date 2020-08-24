796c3b3aeea788acec83d22eeb8cc39878e73d01
leeanh38 <45936728+leeanh38@users.noreply.github.com>
2020-08-24 17:30:56 +0700

    Add files via upload

---
+++ src/java/DBContext/DBContext.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Admin
+ */
+public class DBContext {
+    public Connection getConnection()throws Exception {
+        String url = "jdbc:sqlserver://"+serverName+":"+portNumber +";databaseName="+dbName;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }
+    
+    /*Insert your other code right after this comment*/
+   
+    /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
+    private final String serverName = "localhost";
+    private final String dbName = "PRJ321_Ass1";
+    private final String portNumber = "1433";
+    private final String userID = "sa";
+    private final String password = "123";
+}
